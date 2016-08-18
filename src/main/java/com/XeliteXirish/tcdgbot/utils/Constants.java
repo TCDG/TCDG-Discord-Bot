d705636d05663807e07aadedab0724c326826ebc
XeliteXirish <shaunoneill7@me.com>
2016-08-18 20:54:56 +0100

    Added myself to owners

--- src/main/java/com/XeliteXirish/tcdgbot/utils/Constants.java
+++ src/main/java/com/XeliteXirish/tcdgbot/utils/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
 
     public static final String COMMAND_PREFIX = "/";
 
-    public static final String TCDG_OWNERS_URL = "";
+    public static final String TCDG_OWNERS_URL = "https://raw.githubusercontent.com/TCDG/TCDG-Discord-Bot/master/tcdg_user_owners.json";
 }
